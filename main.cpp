11a4ebf2f62b47dca963bb82423322db6d5faa01
Jordan <J83001@gmail.com>
2017-03-25 17:41:22 -0500

    Added Torso and Locomotive classes

--- main.cpp
+++ main.cpp
@@ -89,6 +89,83 @@ Arm::Arm(string in_name,int in_model_number,string in_description,
 }
 
 double Arm::get_max_power() {return max_power;}
+
+// Torso Class
+
+class Torso:public Robot_Part
+{
+private:
+  int battery_compartments;
+  int max_arms;
+public:
+  int get_battery_compartments();
+  int get_max_arms();
+  Torso();
+  Torso(string, int, string, string, double, double, int, int);
+  
+
+};
+
+Torso::Torso():Robot_Part()
+{
+
+  battery_compartments = 0;
+  max_arms = 0;
+
+}
+
+Torso::Torso(string in_name,
+	     int in_model_number, string in_description,
+	     string in_image_filename, double in_cost,
+	     double in_weight, int in_max_arms,
+	     int in_battery_compartments):Robot_Part(in_name,
+		       in_model_number, in_description,
+		       in_image_filename, in_cost, in_weight)
+{
+  battery_compartments = in_battery_compartments;
+  max_arms = in_max_arms;
+
+}
+    
+// Locomotor Class
+
+class Locomotor:public Robot_Part
+{
+private:
+  double max_power;
+  double max_speed;
+public:
+  Locomotor();
+  Locomotor(string, int, string, string, double,double, double, double);
+  double get_max_speed();
+  double get_max_power();
+  
+};
+
+Locomotor::Locomotor()
+{
+
+  max_power = 0;
+  max_speed = 0;
+  
+}
+
+Locomotor::Locomotor(string in_name,
+		       int in_model_number, string in_description,
+		       string in_image_filename, double in_cost,
+		     double in_weight, double in_max_speed,
+		     double in_max_power):Robot_Part(in_name,
+		     in_model_number, in_description,
+		     in_image_filename, in_cost, in_weight)
+{
+
+  max_speed = in_max_speed;
+  max_power = in_max_power;
+  
+}
+				       
+
+
   
 int main(void)
 {
