65497037295c124e98497703ceeb9b6628115697
Jordan <J83001@gmail.com>
2017-03-27 15:32:51 -0500

    Start View and also PHB

---
+++ PHB.cpp
@@ -0,0 +1,19 @@
+class PHB:
+{
+private:
+  int total_sales; // The number of sales
+  int total_profits;
+  vector<Sales_Associate> sales_associates;
+  vector<unprocessed_orders> unprocessed_orders;
+  vector<processed_orders> processed_orders;
+  double average_profit_margin;
+
+public:
+  void give_raise(Sales_Associate);
+
+  
+  
+
+
+
+}
